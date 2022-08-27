07be2cb4e3248123bb7efec16d86028ded98fd35
sabrinaporto <sabrinapmota@yahoo.com.br>
2022-08-27 17:58:19 -0300

    adicionado método para verificação de satatus do pagamento

--- contasapagar/src/main/java/com/gerenciadordecontas/contasapagar/services/ContasaPagarServices.java
+++ contasapagar/src/main/java/com/gerenciadordecontas/contasapagar/services/ContasaPagarServices.java
@@ -1,6 +1,7 @@
 package com.gerenciadordecontas.contasapagar.services;
 
 import com.gerenciadordecontas.contasapagar.model.ContasaPagarModel;
+import com.gerenciadordecontas.contasapagar.model.enums.Status;
 import com.gerenciadordecontas.contasapagar.repository.ContasaPagarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,13 @@ public class ContasaPagarServices {
     }
 
     public ContasaPagarModel cadastrarconta(ContasaPagarModel contasaPagarModel) {
+
+        LocalDate dataAtual = LocalDate.now();
+        if (contasaPagarModel.getDataVencimento().isBefore(dataAtual)) {
+            contasaPagarModel.setStatus(Status.VENCIDO);
+        } else {
+            contasaPagarModel.setStatus(Status.AGUARDANDO);
+        }
         contasaPagarModel.getId();
         contasaPagarModel.getDataVencimento();
         contasaPagarModel.getNome();
@@ -31,13 +39,20 @@ public class ContasaPagarServices {
         return contasaPagarRepository.save(contasaPagarModel);
     }
 
-    public ContasaPagarModel  alterarContas(ContasaPagarModel contasaPagarModel) {
+    public ContasaPagarModel alterarContas(ContasaPagarModel contasaPagarModel) {
+
+        if (contasaPagarModel.getStatus().equals(Status.PAGO)) {
+            LocalDateTime dataAtual = LocalDateTime.now();
+            contasaPagarModel.setDataPagamento(dataAtual);
+        }
+
         contasaPagarModel.getDataPagamento();
         contasaPagarModel.getValor();
         contasaPagarModel.getTipo();
-                contasaPagarModel.getStatus();
-                return contasaPagarRepository.save(contasaPagarModel);
+        contasaPagarModel.getStatus();
+        return contasaPagarRepository.save(contasaPagarModel);
     }
+
     public void deletarcontas(Long id) {
         contasaPagarRepository.deleteById(id);
     }
